118eda611730fa2256f18201e97d9a65cea063f9
nurfatinnblh14 <47810583+nurfatinnblh14@users.noreply.github.com>
2019-09-10 15:33:36 +0800

    Add files via upload

---
+++ version 2/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = '4015';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername,$databasePassword, $databaseName );
+?>
